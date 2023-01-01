e773dc0bad9b1957e42ff1d7714cdf09dec634f6
Eko Kurniadi <eko.kurniadi@majoo.id>
2023-01-01 23:13:35 +0700

    deklarasi variabel dan print isi variabel

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
+    let variable_name ="deklarasi variable";
+    println!("{}",variable_name);
     println!("Hello, world!");
     println!("How are you");
     println!("First Rush Application")
